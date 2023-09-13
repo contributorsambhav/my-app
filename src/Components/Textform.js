fc7e6c83a51a50ce0b2cc7bfc0cf9e8b1dff33e0
contributorsambhav <Sambhav511974@gmail.com>
2023-09-13 23:32:35 +0530

    Textform created and CSS applied

---
+++ src/Components/Textform.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Textform() {
+  return (
+    <div>
+<div className="tackxt">
+  <label for="exampleFormControlTextarea1" class="form-label"></label>
+  <textarea className="form-control tackxt" placeholder='Enter content here: ' id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+    </div>
+  )
+}
